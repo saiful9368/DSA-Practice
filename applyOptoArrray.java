4315aa4e58b2ce99f03cae52a3f2827a3adada85
Saiful Rahman <114799928+saiful9368@users.noreply.github.com>
2025-03-03 22:56:00 +0530

    Create applyOptoArrray.java
    Apply Operations to an Array
    You are given a 0-indexed array nums of size n consisting of non-negative integers.
    
    You need to apply n - 1 operations to this array where, in the ith operation (0-indexed), you will apply the following on the ith element of nums:
    
    If nums[i] == nums[i + 1], then multiply nums[i] by 2 and set nums[i + 1] to 0. Otherwise, you skip this operation.
    After performing all the operations, shift all the 0's to the end of the array.
    
    For example, the array [1,0,2,0,0,1] after shifting all its 0's to the end, is [1,2,1,0,0,0].
    Return the resulting array.
    
    Note that the operations are applied sequentially, not all at once.

---
+++ applyOptoArrray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+        int slow = 0; 
+        for (int fast = 0; fast < n; fast++) {
+            if (nums[fast] != 0) {
+                int temp = nums[fast];
+                nums[fast] = nums[slow];
+                nums[slow] = temp;
+                slow++;
+            }
+        }
+
+        return nums;
+    }
+}
